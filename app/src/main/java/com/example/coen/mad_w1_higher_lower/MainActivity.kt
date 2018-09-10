fcaf41dab554776de72e03245f66b297f1d31a50
Coen Onderwater <coenonderwater@gmail.com>
2018-09-10 11:59:02 +0200

    feature: add comments

--- app/src/main/java/com/example/coen/mad_w1_higher_lower/MainActivity.kt
+++ app/src/main/java/com/example/coen/mad_w1_higher_lower/MainActivity.kt
@@ -20,14 +20,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setScore(score)
-        setHighScore(highScore)
-        throwDice()
         fab_higher.setOnClickListener { higherClick() }
         fab_lower.setOnClickListener { lowerClick() }
         rv_results.layoutManager = LinearLayoutManager(this)
+
+        // Declare inition scores
+        setScore(score)
+        setHighScore(highScore)
+
+        // Throuw dice to start the first round
+        throwDice()
     }
 
+    /**
+     * Throw the dice and set the correct image resource.
+     */
     private fun throwDice(): Int {
         dice = Random().nextInt(7 - 1) + 1
         iv_dice.setImageResource(dices[dice-1])
@@ -35,59 +42,79 @@ class MainActivity : AppCompatActivity() {
         return dice
     }
 
+    /**
+     * Set the score text in the view.
+     */
     private fun setScore(value: Int) {
         tv_score.text = resources.getString(R.string.score, value)
     }
 
+    /**
+     * Set the highscore text in the view
+     */
     private fun setHighScore(value: Int) {
         highScore = value
         tv_highscore.text = resources.getString(R.string.highscore, value)
     }
 
+    /**
+     * Play round.
+     * Triggered on higher button.
+     */
     private fun higherClick() {
+        // store previous throw and throw the dice.
         val prevThrow = dice
         throwDice()
 
-        if (dice > prevThrow) {
-            winAction()
-        } else {
-            loseAction()
-        }
+        // If the new throw is higher than the previous trigger a win
+        if (dice > prevThrow) winAction()
+        else loseAction()
     }
 
+    /**
+     * Play round.
+     * Triggered on lower button.
+     */
     private fun lowerClick() {
         val prevThrow = dice
         throwDice()
 
-        if (dice < prevThrow) {
-            winAction()
-        } else {
-            loseAction()
-        }
+        // If the new throw is lower than the previous trigger a win
+        if (dice < prevThrow) winAction()
+        else loseAction()
     }
 
+    /**
+     * Increase the score and notify the user that the round is won.
+     */
     private fun winAction() {
         setScore(++score)
-        if (score > highScore) {
-            setHighScore(score)
-        }
-
+        // Update if new highscore is reached
+        if (score > highScore) setHighScore(score)
         snackbarMsg("You win")
     }
 
+    /**
+     * Reset the score and notify the user.
+     */
     private fun loseAction() {
         score = 0
         setScore(0)
         snackbarMsg("You lose")
     }
 
-
+    /**
+     * Add new throw to the list.
+     */
     private fun addThrow() {
         println("add throw: " + dice)
         throws.add(dice)
         rv_results.adapter = ListAdapter(throws, this)
     }
 
+    /**
+     * Use the snackbar to display messages.
+     */
     private fun snackbarMsg(value: String) {
         Snackbar.make(fab_lower, value, Snackbar.LENGTH_SHORT).show()
     }
